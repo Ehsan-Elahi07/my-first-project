3d8e03bfdcde74bec16436ec6176be7d7dc908f8
Ehsan-Elahi07 <balghariehsanelahi008@gmail.com>
2024-07-17 23:50:25 +0500

    first project js

---
+++ index.js
@@ -0,0 +1,194 @@
+// alert("Your form has been submitted")
+// var name = "Ahsan";
+// console.log(name)
+// var name ="Ahsan";
+// var myName ="Amanullah"
+// alert(myName)
+// var name;
+// name="Ahsan";
+// window.alert(name)
+// console.log(name)
+// var num = 123;
+// alert(num)
+// var message = "Thanks for your response"
+// alert(message)
+// console.log(message)
+// var num = 123;
+// var num2= num + 10;
+// var num3 = num2 + 5;
+// alert(num3);
+// var num1 = 5;
+// var num2 = 10;
+// var totalNum= num1 + num2;
+// alert(totalNum);
+// var num1 = 5;
+// num1 = num1 + 10;
+// alert(num1)
+// var num1 = 15;
+// var num2 = 5;
+// var finalNumber = num1 - num2;
+// // alert(finalNumber)
+
+// var num1 = "10";
+// var num2 = 5;
+// var num3 = num1 + num2;
+// alert(num3);
+
+// var name1 = "Ahsan";
+// var name2 = "Sohail";
+// var broName = "Aman"
+// var name3 = name1 +" " + name2+ " " + broName;
+// // alert(name3)
+// console.log(name3)
+// var name = "Ahsan Sohail Aman";
+// alert(name)
+
+// Variable name legal & illegal
+// var Class = 12
+// var var = "abc"
+// var alert = "jkdjf"
+// var abc xyz
+// var studen_class
+// var rupees$
+// var rose = "subflower"
+// console.log(rose);
+// var name1 = "abc"
+// var 1name
+// var $name
+// var student _name
+// var studentName = "ali"   camelCase
+// var num; variable declare
+// num = 4; variable assign
+// var num1 = 10;
+// var num1 = 5 + 5
+// var num1 = 5;
+// var num2 = num1 + 5
+// var num2 = 10;
+// var sumNumber = num1 + num2;
+// var num1 = 10;
+// var num2 = 5
+// var subNum = num1 - num2
+// alert(num1 - num2);
+// var num1 = 5;
+// var num2 =6;
+// var num3 = num1 * num2;
+// alert(num3)
+// var num1 = 10;
+// var num2 = 2;
+// var divideNum = num1/num2;
+// alert(divideNum);
+
+// var remainderNum = 10%2;
+// alert(remainderNum);
+
+//  find this number is even or odd
+//  var num1=10;
+//  var num2 = 10%2
+//  if(num2 == 0){
+//     console.log("this number is even")
+//  }
+//  else{
+//     console.log("this number is odd")
+//  }
+// var num1 = 10
+// var num2 = 5
+// var mul = num1% num2
+// alert(mul);
+// var userInput = prompt("Enter your number")
+// var num = userInput;
+// alert(num);
+
+
+// Basic arthmatic calculator
+// var num1=prompt("Enter Your first Number");
+// var operator = prompt("Enter Your Operator", "+,-,*,/");
+// var num2=prompt("Enter Your second  Number");
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2);
+// if(operator== "+"){
+//  var sum = num1 + num2;
+//  document.write(sum);
+// }
+// else if (operator== "-") {
+//     var subtract = num1 - num2;
+//     console.log(subtract);
+// } else if(operator == "*") {
+//     var mul = num1 * num2;
+//     console.log(mul)
+// }else{
+//     var divide = num1 / num2;
+//     console.log(divide);
+// }
+
+
+// math expression with unfamiliar operator
+// var num =1;
+// var newNum = num++
+// var num1 = newNum +1;
+// console.log(num);
+// console.log(newNum);
+// console.log(num1)
+
+// var num = 1;
+// var newNum = --num;
+// console.log(num);
+// console.log(newNum);
+
+// for(i=10; i>=5; i--){
+
+// }
+// eliminating embiguity
+
+// var num = (1+4)*4;
+// console.log(num);
+// var num = 1+(3*4);
+// console.log(num)
+// var resultOfComputation = ((2 * 4) * 4) + 2;
+// var resultOfComputation = (2 * 4) * (4 + 2);
+// // console.log(resultOfComputation);
+// var text1 = "thanks"
+// var user = "Ahsan";
+// var exclamation= "!";
+// alert(text1 + user + exclamation);
+// console.log("thanks"+ " " + "Ahsan"+" " + "!")
+// console.log(text1+" "+user+" "+exclamation)
+// var num1 = "2";
+// var num2 = "2";
+// console.log(num1+num2);
+// console.log("2 plus 2 is equal to "+(2+2))
+// var user= prompt("Your Name","Ali")
+// console.log(user)
+
+// IF STATEMENT START HERE
+// var person = prompt("Where does Ahsan live");
+// if(person=="Balghar"){
+//   alert("Correct Answer")
+// }else{
+//     alert("Incorrect Answer")
+// }
+ 
+// CHECK THESE TWO NUMBER IS EQUAL OR NOT
+var score=1
+var num1 = prompt("Enter your fist number");
+var num2 = prompt("Enter your second number");
+if(num1==num2){
+    alert("These two number is equal")
+    score++;
+}else{
+    alert("not equal")
+}
+console.log(score);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
